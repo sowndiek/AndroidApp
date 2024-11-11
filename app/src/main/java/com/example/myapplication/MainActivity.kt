6cda9a0c98c3930f00077f354b5bd4a7615521fe
mariia <lytvynmaria10@gmail.com>
2024-11-11 17:40:59 +0200

    Working bars for Email and Password, clickable button Next

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val button = findViewById<Button>(R.id.buttonNext)
+
+        button.setOnClickListener {
+            val intent = Intent(this, Create_Account2::class.java)
+            startActivity(intent)
         }
     }
 }
