443d3d32ffed96bc5c25f3d84729df60db0aebb9
yedoka <akedilbekbolatov@gmail.com>
2024-12-11 17:52:50 +0100

    small adjustment

--- app/src/main/java/com/example/myapplication/LoginFragment.kt
+++ app/src/main/java/com/example/myapplication/LoginFragment.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import  androidx.fragment.app.Fragment
+import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputLayout
 class LoginFragment(val credentialsManager: CredentialsManager) : Fragment() {
     private var listener: EventsInterface? = null
