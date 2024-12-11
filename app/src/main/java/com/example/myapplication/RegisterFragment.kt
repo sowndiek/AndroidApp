27d044242eb9a8cc0343c795a79269483c8e3f6b
yedoka <akedilbekbolatov@gmail.com>
2024-12-11 17:56:10 +0100

    small adjustment

--- app/src/main/java/com/example/myapplication/RegisterFragment.kt
+++ app/src/main/java/com/example/myapplication/RegisterFragment.kt
@@ -7,7 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import  androidx.fragment.app.Fragment
+import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputLayout
 import com.example.myapplication.LoginFragment.EventsInterface
 
